13f2152554cc9f80621e0f378387d2db6064a4cc
Wilsonb340 <133293474+Wilsonb340@users.noreply.github.com>
2025-10-16 19:38:45 -0500

    Hello commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Models;
+
+namespace MyWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
